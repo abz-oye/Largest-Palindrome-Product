352666fa520067300f8510bb929b5a157df6cdfd
Abdullah Oyedele <abdullahoyedele2019@gmail.com>
2022-12-27 22:21:31 +0000

    Create Problem4.cs

---
+++ Problem4.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for(int a0 = 0; a0 < t; a0++){
+            int n = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}

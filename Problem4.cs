d7c2bf7f09b3f284ec0d09b578fddc9fc001af75
Abdullah Oyedele <abdullahoyedele2019@gmail.com>
2023-01-22 00:42:12 +0000

    Update Problem4.cs

--- Problem4.cs
+++ Problem4.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 class Solution {
-
+    
     static void Main(String[] args) {
-        int t = Convert.ToInt32(Console.ReadLine());
-        for(int a0 = 0; a0 < t; a0++){
-            int n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("hello");
-        }
+        int x, y;
+		string product="" , res="";
+
+		for (x = 100; x <= 999; x++)
+		{
+			for (y = 100; y <= 999; y++)
+			{
+				product = Convert.ToString(x*y);
+
+				if (product == new String(product.Reverse().ToArray()))
+				{
+					Console.WriteLine("X=" + x + " Y=" + y );
+					res = product;
+
+					Console.WriteLine("Polindrome is: " + res);
+				}
+
+				else { continue; }
+			}
+		}
     }
 }
